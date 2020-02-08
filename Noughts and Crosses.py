5f9f76eaf9614ba13f1f8ed5c476e2ffb51f05c4
Piotr Patrzylas <piotrpatrzylas@gmail.com>
2020-02-08 21:02:50 +0000

    OOP implementation

--- Noughts and Crosses.py
+++ Noughts and Crosses.py
@@ -1,43 +1,61 @@
-#Necessary modules
 import pygame
 
-#Pygame initialization
+#Initial Setup
 pygame.init()
 user_width = pygame.display.Info().current_w
-width, height = user_width//6, width+width//10
+width = user_width//6
+height = width+width//10
 frame = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Tic Tac Toe")
 icon = pygame.image.load("tictactoe.ico")
-
-#Images
 x_standard = pygame.image.load("X_standard.png")
 o_standard = pygame.image.load("O_standard.png")
-pygame.display.set_icon(icon)
+vlinecoord = [[x, y] for x in [width // 3, width // 3 * 2] for y in [0, width // 3 * 3]]
+hlinecoord = [[x, y] for y in [width // 3, width // 3 * 2] for x in [0, width // 3 * 3]]
+linecoord = vlinecoord + hlinecoord
+frame.fill((255, 255, 255))
+pygame.draw.rect(frame, (192, 192, 192), (0, width, width, height - width))
+for i in range(0, 8, 2):
+    pygame.draw.line(frame, (0, 0, 0), linecoord[i], linecoord[i + 1])
 
-#Variables and functions
+class NoCGame:
+    def __init__(self, width):
+        self.width = width
+        pass
+
+class Button:
+    def __init__(self, type):
+        self.type = "NewGame"
+    def Draw(self):
+        pass
+
+class Board:
+    def ClearBoard(self):
+        pass
+    def InsertX(self, x,y):
+        frame.blit(x_standard, (x, y))
+        pygame.display.flip()
+    def InsertY(self, x,y):
+        frame.blit(o_standard, (x, y))
+        pygame.display.flip()
+    def Regions(self):
+        pass
+
+Game = NoCGame(user_width)
+Board = Board()
+
+# Main Loop
 running = True
-vlinecoord = [[x,y] for x in [width//3, width//3*2] for y in [0, width//3*3]]
-hlinecoord = [[x,y] for x in [0, width//3*3] for y in [width//3, width//3*2]]
-
-def empty_board():
-    frame.fill((255, 255, 255))
-    pygame.draw.rect(frame, (192, 192, 192), (0, width, width, height - width))
-    pygame.draw.line(frame, (0,0,0), vlinecoord[0], vlinecoord[1])
-    pygame.draw.line(frame, (0,0,0), vlinecoord[2], vlinecoord[3])
-    pygame.draw.line(frame, (0,0,0), hlinecoord[0], hlinecoord[2])
-    pygame.draw.line(frame, (0,0,0), hlinecoord[1], hlinecoord[3])
-empty_board()
-
-#Main Loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mx, my = pygame.mouse.get_pos()
-            print(mx,my)
-           # frame.blit(x_standard, (150,150))
-           # pygame.display.flip()
+            print(mx, my)
+            if mx < 100 and my < 100:
+                x, y = mx, my
+                #Board.InsertX(x, y)
     pygame.display.update()
 
 pygame.quit()
