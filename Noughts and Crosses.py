3fd524496bcfec47bbaf6402f19cd75ab44203aa
Piotr Patrzylas <piotrpatrzylas@gmail.com>
2020-02-15 15:13:03 +0000

    Window, Board updates.

--- Noughts and Crosses.py
+++ Noughts and Crosses.py
@@ -1,5 +1,6 @@
 import pygame
 import tkinter
+import random
 
 #Initial Setup
 pygame.init()
@@ -45,10 +46,24 @@ class Board(Window):
     pygame.display.set_caption("Noughts and Crosses")
     icon = pygame.image.load("tictactoe.ico")
     pygame.display.set_icon(icon)
-    x_standard = pygame.image.load("X_standard.png")
-    o_standard = pygame.image.load("O_standard.png")
+
     def __init__(self):
         super().__init__(uwidth=user_width)
+        self.x_standard = pygame.image.load("X_standard.png")
+        self.x_standard = pygame.transform.scale(self.x_standard, ((self.width//3)-2, (self.width//3)-2))
+        self.o_standard = pygame.image.load("O_standard.png")
+        self.o_standard = pygame.transform.scale(self.o_standard, ((self.width // 3) - 2, (self.width // 3) - 2))
+        self.Regions = {1: [pygame.Rect(0, 0, self.width // 3, self.width // 3), 1, ""], \
+                        2: [pygame.Rect(self.width // 3, 0, self.width // 3, self.width // 3), 1, ""], \
+                        3: [pygame.Rect((self.width // 3)*2, 0, self.width // 3, self.width // 3), 1, ""], \
+                        4: [pygame.Rect(0, self.width // 3, self.width // 3, self.width // 3), 1, ""], \
+                        5: [pygame.Rect(self.width // 3, self.width // 3, self.width // 3, self.width // 3), 1, ""], \
+                        6: [pygame.Rect((self.width // 3)*2, self.width // 3, self.width // 3, self.width // 3), 1, ""], \
+                        7: [pygame.Rect(0, (self.width // 3)*2, self.width // 3, self.width // 3), 1, ""], \
+                        8: [pygame.Rect(self.width // 3, (self.width // 3)*2, self.width // 3, self.width // 3), 1, ""], \
+                        9: [pygame.Rect((self.width // 3)*2, (self.width // 3)*2, self.width // 3, self.width // 3), 1, ""]
+                        }
+
     def ClearBoard(self):
         self.frame.fill((255, 255, 255))
         self.newgameButton = Button("newgame", self.frame, self.width)
@@ -56,38 +71,55 @@ class Board(Window):
         self.infoButton = Button("info", self.frame, self.width)
         for i in range(0, 8, 2):
             pygame.draw.line(self.frame, (0, 0, 0), self.linecoord[i], self.linecoord[i + 1])
+        for i in self.Regions:
+            self.Regions[i][1] = 0
     def InsertX(self, x,y):
         self.frame.blit(self.x_standard, (x, y))
         pygame.display.flip()
-    def InsertY(self, x,y):
+    def InsertO(self, x,y):
         self.frame.blit(self.o_standard, (x, y))
         pygame.display.flip()
-    def Regions(self):
-        pass
 
-CurrentWindow = Window(user_width)
-CurrentBoard = Board()
+class Game():
+    CurrentWindow = Window(user_width)
+    CurrentBoard = Board()
+    Cross = random.choice(["Player", "AI"])
+    Nought = str(list({"AI", "Player"} - {Cross})[0])
+    turn = Cross
 
-# Main Loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mx, my = pygame.mouse.get_pos()
-            print(mx, my)
-            if CurrentWindow.newgameButton.rect.collidepoint(mx,my):
-                print("Clicked New Game")
-                CurrentBoard.ClearBoard()
-            if CurrentWindow.quitButton.rect.collidepoint(mx,my):
-                print("Clicked Quit Game")
+    # Main Loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 running = False
-            if CurrentWindow.infoButton.rect.collidepoint(mx, my):
-                print("Clicked Info")
-            if mx < 100 and my < 100:
-                x, y = mx, my
-                CurrentBoard.InsertX(x, y)
-    pygame.display.update()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mx, my = pygame.mouse.get_pos()
+                print("X: ", mx, "Y: ", my)
+                print(turn)
+                if CurrentWindow.newgameButton.rect.collidepoint(mx, my):
+                    print("Clicked New Game")
+                    CurrentBoard.ClearBoard()
+                    Cross = random.choice(["Player", "AI"])
+                    Nought = str(list({"AI", "Player"} - {Cross})[0])
+                    turn = Cross
+                if CurrentWindow.quitButton.rect.collidepoint(mx, my):
+                    print("Clicked Quit Game")
+                    running = False
+                if CurrentWindow.infoButton.rect.collidepoint(mx, my):
+                    print("Clicked Info")
+                if turn == "Player" and Cross == "Player":
+                    for i in range(1, 10):
+                        if CurrentBoard.Regions[i][0].collidepoint(mx, my) and CurrentBoard.Regions[i][1] == 0:
+                            CurrentBoard.InsertX(CurrentBoard.Regions[i][0][0]+1, CurrentBoard.Regions[i][0][0]+1)
+                            CurrentBoard.Regions[i][1] == 1
 
-pygame.quit()
+                if turn == "Player" and Nought == "Player":
+                    for i in range(1, 10):
+                        if CurrentBoard.Regions[i][0].collidepoint(mx, my) and CurrentBoard.Regions[i][1] == 0:
+                            CurrentBoard.InsertO(CurrentBoard.Regions[i][0][0] + 1, CurrentBoard.Regions[i][0][0] + 1)
+                            CurrentBoard.Regions[i][1] == 1
+
+        pygame.display.update()
+
+    pygame.quit()
